96e954feaa68e6d049b7b52948c773b55f1b89b8
Beatfaas <beat.faas@flecopower.ch>
2026-08-18 13:45:23 +0200

    Initialer Commit

--- my-project/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from './components';
-import { setupUtilities } from './utils';
-
-const app = initializeApp();
-setupUtilities();
-
-console.log('Application initialized:', app);
